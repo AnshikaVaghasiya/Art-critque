0c32ab800b8f027267782734d956a095b4d7e5d0
AnshikaVaghasiya <anshikavaghasiya8001@gmail.com>
2025-07-07 11:29:49 +0530

    Third Commit

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,28 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// function Login() {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const handleLogin = async() => {
-//     try{
-//       const res = await axios.post('http://localhost:5000/api/auth/register');
-//       localStorage.setItem('token', res.data);
-//       alert('Login Successfully');
-//     } catch(err) {
-//       alert('Login failed');
-//     }
-//   };
-//   return(
-//     <div className="p-4 max-w-md mx-auto">
-//       <h2 className="text-2xl font-bold mb-4">Login</h2>
-//       <input value= {email} onChange={e => setEmail(e.target.value)} placeholder ="Email" className="input"/>
-//       <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" className="input"/>
-//       <button type='button' onClick={handleLogin} className="btn">Login</button>
-//     </div>
-//   );
-// }
-// export default Login;
-
 import React, { useState } from 'react';
 import axios from 'axios';
 function Login() {
