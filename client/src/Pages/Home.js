f9facce172a222fc49abc3a228021e885f339574
AnshikaVaghasiya <anshikavaghasiya8001@gmail.com>
2025-07-07 11:27:35 +0530

    Seconf Commit

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,29 +1,3 @@
-// import { motion } from 'framer-motion';
-// function Home() {
-//     return (
-//         <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}}>
-//             <h1 className="text-4x1 font-bold">Welcome to My Portfolio</h1>
-//         </motion.div>
-//     );
-// }
-// export default Home;
-
-// import React from 'react';
-// import { motion } from 'framer-motion';
-// import RegistrationForm from './RegistrationForm'; // adjust path if needed
-
-// function Home() {
-//   return (
-//     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="p-6">
-//       <h1 className="text-4xl font-bold mb-6">Welcome to My Portfolio</h1>
-      
-//       {/* Registration form below */}
-//       <RegistrationForm />
-//     </motion.div>
-//   );
-// }
-
-// export default Home;
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
